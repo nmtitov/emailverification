3ecb16149a589ba0dc25ef24dbe18d487d38c7af
Nikita Titov <nmtitov@yandex.ru>
2018-03-28 19:31:15 +0200

    Create UIColor+EmailVerification

---
+++ EmailVerification/Misc/ProjectSpecific/UIColor+EmailVerification.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+EmailVerification.h
+//  EmailVerification
+//
+//  Created by Nikita Titov on 28/03/2018.
+//  Copyright © 2018 N. M. Titov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (EmailVerification)
+
++ (instancetype)email_verification__is_valid_text;
++ (instancetype)email_verification__is_not_valid_text;
+
+@end
