949dc26735c93a92e5baf2e329e1022e2aba8d01
Nikita Titov <nmtitov@yandex.ru>
2018-03-28 18:40:15 +0200

    Create EmailValidator stub

---
+++ EmailVerification/Misc/Common/EmailValidator.m
@@ -0,0 +1,21 @@
+//
+//  EmailValidator.m
+//  EmailVerification
+//
+//  Created by Nikita Titov on 28/03/2018.
+//  Copyright © 2018 N. M. Titov. All rights reserved.
+//
+
+#import "EmailValidator.h"
+
+@implementation EmailValidator
+
+- (instancetype)init {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    return self;
+}
+
+@end
