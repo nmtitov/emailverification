e3f06a33889921bd72c43f926be1fef9170d17f4
Nikita Titov <nmtitov@yandex.ru>
2018-03-28 18:19:00 +0200

    Create MasterViewModel stub

---
+++ EmailVerification/ViewModel/MasterViewModel.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewModel.h
+//  EmailVerification
+//
+//  Created by Nikita Titov on 28/03/2018.
+//  Copyright © 2018 N. M. Titov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MasterViewModel : NSObject
+
+@end
---
+++ EmailVerification/ViewModel/MasterViewModel.m
@@ -0,0 +1,13 @@
+//
+//  MasterViewModel.m
+//  EmailVerification
+//
+//  Created by Nikita Titov on 28/03/2018.
+//  Copyright © 2018 N. M. Titov. All rights reserved.
+//
+
+#import "MasterViewModel.h"
+
+@implementation MasterViewModel
+
+@end
